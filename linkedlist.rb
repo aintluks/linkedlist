07c021308c4116ffa455010035142f74ae712d5c
Lucas da Costa <aintluks@gmail.com>
2021-08-03 12:11:58 -0300

    add: linkedlist insert

--- linkedlist.rb
+++ linkedlist.rb
@@ -5,4 +5,28 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def insert(data)
+    new_node = Node.new(data: data, next_node: nil)
+    
+    unless @head
+      @head = new_node
+    else
+      unless @head.next
+        @head.next = new_node
+      else
+        last = find_last
+        last.next = new_node
+      end
+    end
+  end
+
+  private
+  def find_last
+    list = @head
+    while list
+      return list unless list.next
+      list = list.next
+    end
+  end
 end
