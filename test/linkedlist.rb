c7c935d7d8d61d0f0d362d5a9a472487727499e0
Lucas da Costa <aintluks@gmail.com>
2021-08-03 15:13:44 -0300

    add: test from one to three nodes inserted

--- test/linkedlist.rb
+++ test/linkedlist.rb
@@ -6,10 +6,38 @@ class LinkedListTest < Minitest::Test
     @node = LinkedList.new
   end
 
+  def test_init_linkedlist
+    assert_nil @node.head
+  end
+
   def test_insert_first_node
     new_node = @node.insert('First node')
 
+    assert_nil new_node.next
     assert_equal new_node, @node.head
+    assert_equal 'First node', new_node.data
     assert_equal Node, new_node.class
+    assert_equal 1, @node.length
+  end
+
+  def test_insert_two_nodes
+    new_node_one = @node.insert('First node')
+    new_node_two = @node.insert('Second node')
+
+    assert_nil new_node_two.next
+    assert_equal new_node_one, @node.head
+    assert_equal 'Second node', new_node_two.data
+    assert_equal 2, @node.length
+  end
+
+  def test_insert_three_nodes
+    new_node_one = @node.insert('First node')
+    new_node_two = @node.insert('Second node')
+    new_node_three = @node.insert('Third node')
+
+    assert_nil new_node_three.next
+    assert_equal new_node_one, @node.head
+    assert_equal 'Third node', new_node_three.data
+    assert_equal 3, @node.length
   end
 end
